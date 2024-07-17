2a10cc940c3ff2ae16a70f66fa5af24c409ded04
Manar Abu-Tair <manarabutair481@gmail.com>
2024-07-17 19:01:55 +0300

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std; 
+#define pb push_back
+
+int main(){
+
+int num; 
+int b; 
+int arr[3]={1, 3, 5}; 
+vector<int> c;
+c.pb(arr[2]);
+arr[2]=100;
+float r=500;
+
+return 0;
+}
